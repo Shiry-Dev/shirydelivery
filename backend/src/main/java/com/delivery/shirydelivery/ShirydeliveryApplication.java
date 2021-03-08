52154291c6abf3960363d05bf174a96737cc2ce1
Shiryu Arrien <garrien1@gmail.com>
2021-03-08 16:31:59 -0400

    Project created

---
+++ backend/src/main/java/com/delivery/shirydelivery/ShirydeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.shirydelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirydeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShirydeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/delivery/shirydelivery/ShirydeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.shirydelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShirydeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
